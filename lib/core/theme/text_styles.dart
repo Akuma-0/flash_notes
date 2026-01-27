9fb3e46f417128e0c2d80c660e8c8910d96163f0
Hany <Hanym7md90@gmail.com>
2026-01-28 01:58:49 +0200

    feat: Add custom fonts and text styles to the project

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TextStyles {
+  TextStyles._();
+  //*************** 48 **************//
+  static const TextStyle font48W400 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+  );
+}
