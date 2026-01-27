3c55fad19b7890d0cfc9200466c1fa575d9a19e7
Hany <Hanym7md90@gmail.com>
2026-01-28 01:24:25 +0200

    feat: Initialize Flutter project with iOS and web support
    - Added iOS project files including AppDelegate, Info.plist, and launch assets.
    - Created initial Flutter app structure with main entry point and basic UI.
    - Included necessary assets for app icons and launch images.
    - Set up web support with index.html and manifest.json for PWA capabilities.
    - Added test files for widget testing and basic app functionality.

---
+++ lib/flash_notes_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlashNotesApp extends StatelessWidget {
+  const FlashNotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flash Notes');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flash_notes/flash_notes_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlashNotesApp());
+}
